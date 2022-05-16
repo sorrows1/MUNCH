5e2a27c11d465de13e503250bfadf35608d2f78b
sorrows1 <tanguanyu750@gmail.com>
2022-05-16 18:20:34 +0800

    testing express api

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 export const Navigation = () => {
   return (
     <>
-      <Link to={'/'}>Root</Link>
-      <a href='http://localhost:5000'>Test</a>
     </>
   );
 };
--- server/app.js
+++ server/app.js
@@ -5,22 +5,11 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 const recipeRoutes = require('./routes/recipeRoute.api');
-const mainRoute = require('./routes/mainRoute');
 
 
 app.use(express.json());
 app.use(cors());
-app.engine(
-  'handlebars',
-  exphbs.engine({
-    defaultLayout: 'main', // Specify default template views/layout/main.handlebar
-  })
-);
-app.set('view engine', 'handlebars');
-// Creates static folder for publicly accessible HTML, CSS and Javascript files
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mainRoute);
 app.use('/api/v1/recipes', recipeRoutes);
 
 module.exports = app;
