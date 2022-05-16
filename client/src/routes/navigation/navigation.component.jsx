36f0d9fe9fe7a90607036ce89141bf3b762d8c10
sorrows1 <tanguanyu750@gmail.com>
2022-05-16 18:08:48 +0800

    connected react with express views

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,10 @@
 import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
-  const redirect = () => {
-    window.location.href = 'http://localhost:5000/test';
-  };
   return (
     <>
       <Link to={'/'}>Root</Link>
-      <div onClick={redirect}>Test</div>
+      <a href='http://localhost:5000'>Test</a>
     </>
   );
 };
