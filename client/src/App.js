a3c253d5ce5b543a80e7223481d2f2f09ad17bc2
sorrows1 <tanguanyu750@gmail.com>
2022-06-25 22:47:22 +0800

    merged with everyone

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 
-<<<<<<< HEAD
 import { Navigation } from './routes/navigation.component';
 import Home from './pages/Home.component';
 import Shop from './pages/Shop.component';
